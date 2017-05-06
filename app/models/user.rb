bf3360210bed67278978ab98352ac5f0168c13ba
Lucas <lucasneosr@gmail.com>
2017-05-06 22:21:36 +0800

    added files for account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,4 +33,16 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
+  # Converts email to lowercase
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # Creates and assigns the activation token and digest
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
